157af7fbe922044c9ba8b42f2c97436ab2e11b25
Cristian <123cristianjose@gmail.com>
2025-09-17 18:18:34 -0500

    void components

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -7,9 +7,13 @@ import { ButtonComponent } from './components/button/button.component';
 import { User } from './services/user/user';
 import { RouterModule } from '@angular/router';
 import { ActionSheet } from './providers/actionSheet/action-sheet';
+import { ToggleTranslateComponent } from './components/toggleTranslate/toggle-translate.component';
+import { LinkComponent } from './components/link/link.component';
+import { CardComponent } from './components/card/card.component';
+import { FloatingButtonComponent } from './components/floatingButton/floating-button.component';
 
 const modules = [ CommonModule, FormsModule, ReactiveFormsModule, IonicModule, RouterModule ];
-const components = [ InputComponent, ButtonComponent ];
+const components = [ InputComponent, ButtonComponent, ToggleTranslateComponent, LinkComponent, CardComponent, FloatingButtonComponent ];
 const providers = [ User, ActionSheet ];
 
 @NgModule({
