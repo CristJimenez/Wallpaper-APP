57cefcdd4e75a4473404304f0cdf55cab0da27c8
Cristian <123cristianjose@gmail.com>
2025-09-16 21:39:39 -0500

    supabase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,4 +8,8 @@ export const environment = {
     messagingSenderId: "344877992351",
     appId: "1:344877992351:web:5a53e5efab534fc27895aa"
   },
+  SUPABASE: {
+    URL: 'https://vdheuzqoeepoguhtktsl.supabase.co',
+    API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkaGV1enFvZWVwb2d1aHRrdHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNzU0NTAsImV4cCI6MjA3MzY1MTQ1MH0.SKxF927rsAbEXGQ71NvBQe0PYnSyXPnml8IF7kHdpKU',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-    FIREBASE_CONFIG: {
+  FIREBASE_CONFIG: {
     apiKey: "AIzaSyCHTVR9LSxCZoRwGeHlj9jfcHyr7rx-vF0",
     authDomain: "wallpaper-app-b82bd.firebaseapp.com",
     projectId: "wallpaper-app-b82bd",
@@ -12,6 +12,10 @@ export const environment = {
     messagingSenderId: "344877992351",
     appId: "1:344877992351:web:5a53e5efab534fc27895aa"
   },
+  SUPABASE: {
+    URL: 'https://vdheuzqoeepoguhtktsl.supabase.co',
+    API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkaGV1enFvZWVwb2d1aHRrdHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNzU0NTAsImV4cCI6MjA3MzY1MTQ1MH0.SKxF927rsAbEXGQ71NvBQe0PYnSyXPnml8IF7kHdpKU',
+  }
 };
 
 /*
